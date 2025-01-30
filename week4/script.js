cbf32eb7509b2b05577a700eb411ec75297e983a
doris <chenx16@rose-hulman.edu>
2025-01-30 11:50:17 -0800

    Implement powers of 2 using a while loop

--- week4/script.js
+++ week4/script.js
@@ -2,3 +2,10 @@ console.log("Powers of 2 using for loop:");
 for (let i = 2; i <= 1024; i *= 2) {
   console.log(i);
 }
+
+console.log("Powers of 2 using while loop:");
+let num = 2;
+while (num <= 1024) {
+  console.log(num);
+  num *= 2;
+}
