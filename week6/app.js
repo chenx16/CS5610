20b8c4096338c8b21917ab1539691ce79cff35c8
doris <chenx16@rose-hulman.edu>
2025-02-13 12:33:08 -0800

    Create the public Folder and about.html and Access the HTML Page

--- week6/app.js
+++ week6/app.js
@@ -28,6 +28,9 @@ console.log('Logger version:', logger.version);
 const express = require('express');
 const app = express();
 
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
 // Root route
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -38,9 +41,22 @@ app.get('/tasks', (req, res) => {
     res.send('<h1>List of all the tasks</h1>');
 });
 
+// Route to handle taskId parameter
+app.get('/tasks/:taskId', (req, res) => {
+    const taskId = req.params.taskId; // Extract taskId from the route
+    res.send(`You are viewing task ${taskId}`);
+});
+
+app.get('/tasks/:taskId/:userId', (req, res) => {
+    const { taskId, userId } = req.params; // Extract taskId and userId
+    res.send(`You are viewing task ${taskId} for user ${userId}`);
+});
+
+
 // Start the server
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
 
