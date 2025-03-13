4383cc1f54ead92d7faafc618228278e80d6ea04
doris <chenx16@rose-hulman.edu>
2025-03-13 11:42:58 -0700

    Add delete icon to Task component using react-icons

--- week9/vite-project/src/components/Task.jsx
+++ week9/vite-project/src/components/Task.jsx
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
+import { FaTrash } from "react-icons/fa";
 
 function Task({ task }) {
   return (
-    <li>
+    <li className="task-item">
       <div className="task-container">
-        <p><strong>{task.title}</strong></p>
-        <p>{task.date}</p>
+        <div className="task-content">
+          <p><strong>{task.title}</strong></p>
+          <p>{task.date}</p>
+        </div>
+        <FaTrash className="delete-icon" />
       </div>
     </li>
   );
 }
 
-// Define PropTypes for type-checking
+// Define PropTypes
 Task.propTypes = {
   task: PropTypes.shape({
     id: PropTypes.number.isRequired,
