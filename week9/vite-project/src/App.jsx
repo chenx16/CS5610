b32a3efc208b8466c96cba967323a51d55a636fe
doris <chenx16@rose-hulman.edu>
2025-03-13 12:08:05 -0700

    Implement conditional rendering for AddTask form

--- week9/vite-project/src/App.jsx
+++ week9/vite-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import AddTask from "./components/AddTask";
 import TasksList from "./components/TasksList";
@@ -5,10 +6,18 @@ import TasksList from "./components/TasksList";
 function App() {
   const appName = "Welcome to My App";
 
+  // State to track if the form should be shown
+  const [showForm, setShowForm] = useState(false);
+
+  // Function to toggle form visibility
+  const toggleForm = () => {
+    setShowForm((prev) => !prev);
+  };
+
   return (
     <div className="app-container">
-      <Header appName={appName} />
-      <AddTask />
+      <Header appName={appName} showForm={showForm} onToggleForm={toggleForm} />
+      {showForm && <AddTask />}
       <TasksList />
     </div>
   );
--- week9/vite-project/src/components/Header.jsx
+++ week9/vite-project/src/components/Header.jsx
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
 
-function Header({ appName }) {
+function Header({ appName, showForm, onToggleForm }) {
   return (
     <header className="header">
       <h1>{appName}</h1>
-      <button className="add-task-btn">Add a Task</button>
+      <button className="toggle-btn" onClick={onToggleForm}>
+        {showForm ? "Close" : "Add a Task"}
+      </button>
     </header>
   );
 }
 
-// Define prop types
+// Define PropTypes
 Header.propTypes = {
   appName: PropTypes.string.isRequired,
+  showForm: PropTypes.bool.isRequired,
+  onToggleForm: PropTypes.func.isRequired,
 };
 
 export default Header;
