a2bbe3cce73656312758a331e5e485190de705e3
doris <chenx16@rose-hulman.edu>
2025-04-03 11:36:51 -0700

    Add Auth0ProviderWithHistory component to integrate Auth0 authentication

--- week9/vite-project/src/main.jsx
+++ week9/vite-project/src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import Auth0ProviderWithHistory from "./components/Auth0ProviderWithHistory";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <StrictMode>
+    <Auth0ProviderWithHistory>
       <App />
-    </StrictMode>
+    </Auth0ProviderWithHistory>
   </BrowserRouter>
 );
