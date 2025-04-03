e4148bf7cee06b78a3ebf97b8917feba31e9c111
doris <chenx16@rose-hulman.edu>
2025-04-03 12:05:24 -0700

    Render Auth0 login/logout conditionally with loading spinner

--- week9/vite-project/src/components/AuthenticationButton.jsx
+++ week9/vite-project/src/components/AuthenticationButton.jsx
@@ -1,11 +1,30 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
+import SignupButton from "./SignupButton";
 
 const AuthenticationButton = () => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
 
-  return isAuthenticated ? <LogoutButton /> : <LoginButton />;
+  if (isLoading) {
+    return (
+      <img
+        src="https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg"
+        alt="Loading..."
+        style={{ height: "40px" }}
+      />
+    );
+  }
+
+  // Show Logout if logged in, otherwise show Login + Signup
+  return isAuthenticated ? (
+    <LogoutButton />
+  ) : (
+    <div style={{ display: "flex", gap: "1rem" }}>
+      <LoginButton />
+      <SignupButton />
+    </div>
+  );
 };
 
 export default AuthenticationButton;
