c35a81f63bb5894295526d7e57dcc569be18a686
doris <chenx16@rose-hulman.edu>
2025-04-10 11:04:44 -0700

    Add hamburger menu icon and toggle logic for mobile navigation

--- week9/vite-project/src/components/NavBar.jsx
+++ week9/vite-project/src/components/NavBar.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import AuthenticationButton from "./AuthenticationButton";
-import "./NavBar.css"; // Create this file for styling
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./NavBar.css";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="nav-container">
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/tasks">Tasks</Link>
-        <Link to="/profile">Profile</Link>
-        <AuthenticationButton />
+      <div className="nav-header">
+        <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
       </div>
+
+      {/* Conditionally render links */}
+      {menuOpen && (
+        <div className="nav-links">
+          <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+          <Link to="/tasks" onClick={() => setMenuOpen(false)}>Tasks</Link>
+          <Link to="/profile" onClick={() => setMenuOpen(false)}>Profile</Link>
+          <AuthenticationButton />
+        </div>
+      )}
     </nav>
   );
 }
